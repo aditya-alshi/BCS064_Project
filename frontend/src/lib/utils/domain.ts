9228f197b27a53839d47fa6b0821513a889e56bc
Aditya Alshi <alshiaditya55@gmail.com>
2024-12-05 14:48:36 +0530

    domain name secure removed port number

--- frontend/src/lib/utils/domain.ts
+++ frontend/src/lib/utils/domain.ts
@@ -1,2 +1,2 @@
-export const domain = "https://indiansweetsavories.alshiaditya.com:3000"
+export const domain = "https://indiansweetsavories.alshiaditya.com"
 // export const domain = "http://localhost:5000"
